981cc43d41479c0582c9626aa16a5af40e1315fe
eogkdfh <eogkdfh@gmail.com>
2021-09-14 20:04:25 +0900

    generic parameter

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,20 +2,37 @@ package prob01;
 
 public class Printer {
 	 
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String string) {
+//		System.out.println(string);
+//	}
+		
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 	
-	public void println(boolean b) {
-		System.out.println(b);
-	}
 
-	public void println(double d) {
-		System.out.println(d);
+	public <T> void println(T... ts) {
+		for(T t : ts)
+		   System.out.println(t);
 	}
-
-	public void println(String string) {
-		System.out.println(string);
+	
+	public int sum(Integer... nums) {
+		int sum = 0;
+		for(Integer i : nums) {
+			sum += i;
+		}
+		return sum;
 	}
-		
 }
