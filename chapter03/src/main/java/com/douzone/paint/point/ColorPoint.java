9ddc62724d530f1b2535b04ca3c2947781619a0d
eogkdfh <eogkdfh@gmail.com>
2021-09-14 13:27:07 +0900

    주석 수정

--- chapter03/src/main/java/com/douzone/paint/point/ColorPoint.java
+++ chapter03/src/main/java/com/douzone/paint/point/ColorPoint.java
@@ -17,7 +17,7 @@ public class ColorPoint extends Point {
 		return color;
 	}
 
-	@Override //언어테이션 컴파일러가 오버라이딩가능한지검사 외부에서 사용 스프링부트에서 많이사용
+	@Override //어노테이션 컴파일러가 오버라이딩가능한지검사 외부에서 사용 스프링부트에서 많이사용
 	public void show() {
 		System.out.println("점[x=" + getX() + ", y=" + getY() + ", color = " + getColor() + "]을 그렸습니다.");
 	}
