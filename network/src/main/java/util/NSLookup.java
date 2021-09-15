aba036f276fa66ea02d578068db87b8380b028de
eogkdfh <eogkdfh@gmail.com>
2021-09-15 19:14:36 +0900

    NSLookup

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,17 +2,27 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String line = "www.dozone.com";
-		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName(line);//도메인네임에연결된 모든 주소 반환
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("IP주소를 입력하세요");
+			System.out.print(">>");
+			String line = scanner.nextLine();
+			System.out.println();
+			try {
+				InetAddress[] inetAddresses = InetAddress.getAllByName(line);// 도메인네임에연결된 모든 주소 반환
+                for(InetAddress ia : inetAddresses) {
+                	System.out.println(ia);
+                }
+                System.out.println();
+			} catch (UnknownHostException e) {
+				System.out.println("존재하지 않는 IP주소입니다");
+			}
 		}
 
 	}
